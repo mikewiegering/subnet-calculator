16dac197c825c5fd63f65fa6c794f5eb011a8715
mikewiegering <mike.wiegering@gmail.com>
2020-04-23 04:31:03 +0000

    added infinit loop with q to quit

--- subnet_calculator.py
+++ subnet_calculator.py
@@ -32,60 +32,64 @@ def main():
     # Uncomment the line below the print() welcome message
     print("IP Subnet Calculator")
 
-    # define a variable
-    network = input('Network => ')
+    # define a variable and start an infinite loop
+    network = ''
 
-    # network object
-    network_object = ipaddress.ip_network(network, strict=False)
-    netmask = str(network_object.netmask)
-    netmask_octets = netmask.split('.')
+    while network != 'q':
+        network = input('Network => ')
+        if network == 'q':
+            break
+        else:
 
-    wilcard_octets = []
-    for octet in netmask_octets:
-        wildcard = 255 - int(octet)
-        wilcard_octets.append(str(wildcard))
+         # network object
+            network_object = ipaddress.ip_network(network, strict=False)
+            netmask = str(network_object.netmask)
+            netmask_octets = netmask.split('.')
 
-    dot = '.'
-    wildcard = dot.join(wilcard_octets)
+            wilcard_octets = []
+            for octet in netmask_octets:
+                wildcard = 255 - int(octet)
+                wilcard_octets.append(str(wildcard))
 
-    hosts = list(network_object.hosts())
-    first = hosts[0]
-    last = hosts[-1]
-    num_of_addresses = len(hosts)
+                dot = '.'
+                wildcard = dot.join(wilcard_octets)
 
-    is_private = network_object.is_private
+                hosts = list(network_object.hosts())
+                first = hosts[0]
+                last = hosts[-1]
+                num_of_addresses = len(hosts)
 
-    if is_private:
-        print(f'Network:   {network_object} (private network)')
-    else:
-        print(f'Network:   {network_object}')
-    print(f'Netmask:   {network_object.netmask} = {network_object.prefixlen}')
-    print(f'Wildcard:  {wildcard}')
-    print(f'Broadcast: {network_object.broadcast_address}')
-    print(f'HostMin:   {first}')
-    print(f'HostMax:   {last}')
-    print(f'Hosts/Net: {num_of_addresses}')
+                is_private = network_object.is_private
 
-    print('Would like to to subdivide that network into smaller networks?')
+            if is_private:
+                print(f'Network:   {network_object} (private network)')
+            else:
+                print(f'Network:   {network_object}')
+                print(f'Netmask:   {network_object.netmask} = {network_object.prefixlen}')
+                print(f'Wildcard:  {wildcard}')
+                print(f'Broadcast: {network_object.broadcast_address}')
+                print(f'HostMin:   {first}')
+                print(f'HostMax:   {last}')
+                print(f'Hosts/Net: {num_of_addresses}')
 
-    answer = input()
+# This section asks the user if they would like to subdivide that network into smaller networks
 
-    if answer.lower() == 'yes':
-        print('What size networks would you like them to be?')
-        print(f'The current prefix Length is: {network_object.prefixlen} ')
-        newprefixlen = int(input())
-        new_networks = list(network_object.subnets(new_prefix=newprefixlen))
-        print('The new networks are ')
-        print(*new_networks, sep='\n')
+            print('Would like to to subdivide that network into smaller networks?')
+
+            answer = input()
+
+            if answer.lower() == 'yes':
+                print('What size networks would you like them to be?')
+                print(f'The current prefix Length is: {network_object.prefixlen} ')
+                newprefixlen = int(input())
+                new_networks = list(network_object.subnets(new_prefix=newprefixlen))
+                print('The new networks are ')
+                print(*new_networks, sep='\n')
           
+            else:
+                print('Then I have nothing more to offer you')
 
-
-
-        
-    else:
-        print('Then I have nothing more to offer you')
-
-
+        print('select q as when prompted for the network to quit')
 
 if __name__ == "__main__": 
     main()
